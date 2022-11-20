6dc65e72e6a41877df1e9896076e1dad2f83c39f
justizha <alfathizhaaaaaaa@gmail.com>
2022-11-20 13:49:30 +0700

    smooth

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const Top = document.querySelector("#op");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    Top.classList.remove("hidden");
+    Top.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    Top.classList.remove("flex");
+    Top.classList.add("hidden");
   }
 };
 
